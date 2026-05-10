adb0e0806da3dc842fcdfdc4bb27f7e62e3db4f1
mbalcer <mateusz75319@gmail.com>
2026-05-10 15:36:44 +0200

    Initialize project

---
+++ src/main/kotlin/pl/mbalcer/couponmanagement/CouponManagementApplication.kt
@@ -0,0 +1,11 @@
+package pl.mbalcer.couponmanagement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CouponManagementApplication
+
+fun main(args: Array<String>) {
+    runApplication<CouponManagementApplication>(*args)
+}
